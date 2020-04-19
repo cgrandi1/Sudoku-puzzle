418ca1c634d8b83ffa84b69bd0d30fecd4007865
github username <github email address>
2020-04-19 12:44:24 -0600

    creating positions

--- Sudoku-puzzle-1/app_class.py
+++ Sudoku-puzzle-1/app_class.py
@@ -1,19 +1,38 @@
-import pygame
+import pygame, sys
 from settings import *
 
-class App
+class App:
     def __init__(self)
         pygame.init()
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
+        self.grid = testBoard
+        print(self.grid)
+
 
     def run(self):
         while self.running:
             self.events()
             self.update()
             self.draw()
+        pygame.quit()
+        sys.exit()
 
     def events():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = FALSE
+
+    
+    def update(self):
+        pass
+
+
+    def draw(self):
+        self.window.fill(WIDTH)
+        self.drawGrid(window)
+        pygame.display.update()
+
+    def drawGrid(self):
+        pygame.draw.react(window, BLACK, (gridPos[0], gridPos[1], WIDTH-40, HEIGHT-120))
+
--- Sudoku-puzzle-1/settings.py
+++ Sudoku-puzzle-1/settings.py
@@ -2,3 +2,14 @@ WIDTH = 600
 
 HEIGHT = 600
 
+#Colors
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+
+#Boards
+testBoards = [[0 for x in range(9)] for x in range(9)]
+
+#Positions and sizes
+gridPos = {20,100}
+
+

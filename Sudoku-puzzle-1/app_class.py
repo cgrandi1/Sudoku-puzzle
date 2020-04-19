683e51b923232c90ca20b6cb99f49daeca12372f
github username <github email address>
2020-04-19 17:01:22 -0600

    updating the state

--- Sudoku-puzzle-1/app_class.py
+++ Sudoku-puzzle-1/app_class.py
@@ -6,8 +6,9 @@ class App:
         pygame.init()
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
-        self.grid = testBoard
-        print(self.grid)
+        self.grid = testBoard1
+        self.selected = None
+        self.mousePos =  None
 
 
     def run(self):
@@ -17,21 +18,34 @@ class App:
             self.draw()
         pygame.quit()
         sys.exit()
+    
 
-    def events():
+
+
+    def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = FALSE
-
+            if event.type == pygame.MOUSEDOWN:
+                selected = self.mouseOnGrid()
+                if selected: 
+                    self.selected = selected
+                else:
+                    print("not on board")
+                    self.selected = None
     
     def update(self):
-        pass
-
+        self.mousePos = pygame.mouse.get_pos()
 
     def draw(self):
-        self.window.fill(WIDTH)
+        self.window.fill(WHITE)
+        if self.selected:
+            self.drawSelection(self.window, self.selected)
         self.drawGrid(window)
         pygame.display.update()
+
+    def drawSelection(self, window, pos):
+        pygame.draw.react(window, LIGHTBLUE, ((pos[0]*cellSize], (pos[1]*cellSize)+gridPos[1], cellSize, cellSize ))
 
     def drawGrid(self):
         pygame.draw.react(window, BLACK, (gridPos[0], gridPos[1], WIDTH-150, HEIGHT-150), 2)
@@ -42,3 +56,13 @@ class App:
             else: 
                 pygame.draw.line(window, BLACK, (gridPos[0]+(x*cellSize), gridPos[1]), (gridPos[0]+(x*cellSize), gridPos[1]+450), 2)
                 pygame.draw.line(window, BLACK, (gridPos[0]+(x*cellSize), gridPos[1]+(x*cellSize)), (gridPos[0]+450, gridPos[1]++(x*cellSize)), 2)
+    
+
+def mouseOnGrid(self):
+    if self.mousePos[0] < gridPos[0] or self.mousePos[1] < gridPos[1]:
+        return False
+    if self.mousePos[0] > gridPos[0]+gridSize or self.mousePos[1] > gridPos[1]+gridSize:
+        return False
+    return ((self.mousePos[0]-gridPos[0]//cellSize, (self.mousePos(1)-gridPos[1])//cellSize)
+    
+
--- Sudoku-puzzle-1/settings.py
+++ Sudoku-puzzle-1/settings.py
@@ -1,16 +1,26 @@
 WIDTH = 600
-
 HEIGHT = 600
 
 #Colors
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
+LIGHTBLUE = (96,216,212)
 
 #Boards
-testBoards = [[0 for x in range(9)] for x in range(9)]
+testBoards1 = [[0 for x in range(9)] for x in range(9)]
+testBoards2 = [[0,6,0,2,0,0,8,3,1],
+               [0,0,0,0,8,4,0,0,0],
+               [0,0,7,6,0,3,0,4,9],
+               [0,4,6,8,0,2,1,0,0],
+               [0,0,3,0,9,6,0,0,0],
+               [1,2,0,7,0,5,0,0,6],
+               [7,3,0,0,0,1,0,2,0],
+               [8,1,5,0,2,9,7,0,0],
+               [0,0,0,0,7,0,0,1,5]]
 
 #Positions and sizes
 gridPos = (60,100)
+gridSize = cellSize*9
 
 cellSize = 50
 
